d57e9996ba6c111c18b589a1cc497f320eb36440
akhich551995 <akhich551995@gmail.com>
2024-01-18 15:39:06 -0600

    Added new submit
    we can now request for sample resume points

--- app.py
+++ app.py
@@ -52,16 +52,17 @@ if uploaded_file is not None:
 
 submit1 = st.button("Tell Me about the Resume")
 
-#Submit2 = st.button("How can I improve my resume points?")
-
 submit2 = st.button("Percentage match with Keywords and missing keywords")
+
+submit3 = st.button("Points can be used to fill that missing skills")
 
 input_prompt1 = """
 You are an experienced Human Resource Manager with technical experience in the field of 
 Data Science, Bigdata Engineering, Generativeai, Machine learning Engineer and DataAnalyst.Your task is to review the provided resume against the job description
 for these provided profiles.
 Your task is to share your professional evaluation on the candidate's profile whether it aligns with the job description and 
-Highlight the strengths and weakness of the applicant in relation to the job description.
+Highlight the strengths of the candidate as first output and
+second output should be the necessary skills to be included in the applicants resume with respect to the job description.
 """
 
 input_prompt2 = """
@@ -69,8 +70,17 @@ You are skilled ATS (Applicant Tracking System) scanner with a deep understandin
 Data Science, Bigdata Engineering, Generativeai, Machine learning Engineer and DataAnalyst and deep ATS functionality,
 
 Your task is to evaluate the resume against the job description and give me the percentage of match if the resume matches
-the job description. First output should come as percentage and then Keyword missing as second output.
+the job description. First output should come as percentage and then Keyword missing as bullet point as second output.
 """
+
+input_prompt3 = """
+You are skilled resume builder with a deep understanding of 
+Data Science, Bigdata Engineering, Generativeai,Machine learning Engineer and DataAnalysis.
+
+With the missing skills in job description your task is to provide a sample resume points that can be used to add into the resume
+provide those points in bullets with side heading of that respective skills."""
+
+
 if submit1: 
     if uploaded_file is not None:
         pdf_content = input_pdf_setup(uploaded_file)
@@ -83,6 +93,14 @@ elif submit2:
     if uploaded_file is not None:
         pdf_content = input_pdf_setup(uploaded_file)
         response = get_gemini_response(input_prompt2, pdf_content, input_text)
+        st.subheader("Percentage match and Keywords missing are:")
+        st.write(response)
+    else:
+        st.write("Resume not uploaded")
+elif submit3: 
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt3, pdf_content, input_text)
         st.subheader("Percentage match and Keywords missing are:")
         st.write(response)
     else:
